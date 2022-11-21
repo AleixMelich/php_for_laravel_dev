50f83f99c1f72dcaf06ed5f307c8784be71f45c0
«AleixMelich» <aleixmelich@iesebre.com>
2022-11-21 16:44:25 +0100

    separar configuracio config.ph del codi..

--- app/index.php
+++ app/index.php
@@ -4,17 +4,11 @@ require 'app/helpers.php';
 require 'app/Task.php';
 require 'config.php';
 
+$dbh = new PDO(
+    $config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
+    $config['database']['user'],
+    $config['database']['password']);
 
-
-$user=$config['database']['user'];
-$pass=$config['database']['password'];
-$type = $config['database']['databasetype'];
-$host = $config['database']['host'];
-$name = $config['database']['name'];
-$dsn = "$type:host=$host;dbname=$name";
-
-
-$dbh = new PDO($dsn, $user, $pass);
 $statement = $dbh->prepare('SELECT * FROM tasks;');
 $statement->execute();
 $tasks = $statement->fetchAll(PDO::FETCH_CLASS,'Task');
