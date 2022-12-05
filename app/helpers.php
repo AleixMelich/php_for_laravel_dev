d1ade7f7e2a5ff23b1d01e1de3bdadf1bee91c35
«AleixMelich» <aleixmelich@iesebre.com>
2022-12-05 15:28:56 +0100

    Afegida funcio connectDB

--- app/helpers.php
+++ app/helpers.php
@@ -11,3 +11,9 @@ function dd($xivato){
     var_dump($xivato);
     die();
 }
+function connectDB($config){ //Dependency injection
+    return new PDO(
+        $config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
+        $config['database']['user'],
+        $config['database']['password']);
+}
--- app/index.php
+++ app/index.php
@@ -1,16 +1,16 @@
 <?php
 
+require 'config.php';
 require 'app/helpers.php';
 require 'app/Task.php';
-require 'config.php';
 
-$dbh = new PDO(
-    $config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
-    $config['database']['user'],
-    $config['database']['password']);
+$dbh = connectDB($config);
 
 $statement = $dbh->prepare('SELECT * FROM tasks;');
+
 $statement->execute();
+
+
 $tasks = $statement->fetchAll(PDO::FETCH_CLASS,'Task');
-//var_dump($tasks);
+
 $greeting = greet();
